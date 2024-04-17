ac3446d9720ea09aca8db8187fe4928879576e91
Chris Abdo <chris.j.abdo@gmail.com>
2024-04-17 00:48:54 -0400

    Update layout and page components

--- stemr/app/layout.tsx
+++ stemr/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/lib/theme-provider";
 import Navbar from "@/components/layout/navbar";
 import { Toaster } from "@/components/ui/sonner";
-
-const inter = Inter({ subsets: ["latin"] });
+import { GeistMono } from "geist/font/mono";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+    <html lang="en" className={GeistMono.className} suppressHydrationWarning>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -29,6 +27,7 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+
           <Toaster position="top-center" />
         </ThemeProvider>
       </body>
--- stemr/app/page.tsx
+++ stemr/app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
       <section className="w-full py-6 md:py-12 lg:py-16">
         <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6">
           <div className="space-y-3">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+            <h1 className="text-3xl font-normal tracking-tighter sm:text-4xl md:text-5xl">
               Stemmer
             </h1>
             <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
