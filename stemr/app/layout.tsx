a9c3f5e0330e6dfbc51b107d2c7107b26c168e56
Chris Abdo <chris.j.abdo@gmail.com>
2024-04-17 23:58:17 -0400

    Update global CSS and font in layout component

--- stemr/app/layout.tsx
+++ stemr/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
 import Navbar from "@/components/layout/navbar";
 import { Toaster } from "@/components/ui/sonner";
-import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 export const metadata: Metadata = {
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={GeistMono.className} suppressHydrationWarning>
+    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
